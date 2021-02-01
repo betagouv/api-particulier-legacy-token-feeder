051f45b30dfe82a04201ce003e7d754a1608bc69
Stanislas Bernard <bernardstanislas@gmail.com>
2021-02-01 15:29:28 +0100

    :heavy_plus_sign: Add typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      username: process.env.DB_USER,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
