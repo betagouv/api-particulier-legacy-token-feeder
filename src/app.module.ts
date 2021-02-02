f3965c2879938df8b7b813699577addf97c994ea
Stanislas Bernard <bernardstanislas@gmail.com>
2021-02-02 14:11:53 +0100

    :sparkles: Add ping route

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TokenModule } from 'src/tokens/token.module';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), TokenModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
