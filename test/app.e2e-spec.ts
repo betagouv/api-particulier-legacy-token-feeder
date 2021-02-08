2b6398be2adcf059b51731e1508dba30aedb905d
Stanislas Bernard <bernardstanislas@gmail.com>
2021-02-08 12:23:36 +0100

    :sparkles: Token now holds an apiKey property

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -32,12 +32,15 @@ describe('TokenController (e2e)', () => {
   it('returns a 404 when the API key hash is not known', () => {
     return request(app.getHttpServer()).get('/tokens/croute').expect(404);
   });
-  it('returns the API key hash when it is found', () => {
+  it('returns the token when the API key hash is found', () => {
     return request(app.getHttpServer())
       .get('/tokens/5e42e6a7-3640-4068-b413-0965605efb6d')
       .expect(200)
-      .expect(
-        '57bd72af633a0eadae1cb076e2d1c62621d46b8d65d4fb1856c6f7fb3cd58f9d3da03897762e2748eaeff5f6d358e48515a367220a661a37ac3bea630d6f0ad9',
-      );
+      .expect({
+        id: 'effd3315-487c-4e79-9225-28f303edac2b',
+        hash:
+          '57bd72af633a0eadae1cb076e2d1c62621d46b8d65d4fb1856c6f7fb3cd58f9d3da03897762e2748eaeff5f6d358e48515a367220a661a37ac3bea630d6f0ad9',
+        apiKey: '861aa151-53cd-44c4-843f-563918f402d5',
+      });
   });
 });
